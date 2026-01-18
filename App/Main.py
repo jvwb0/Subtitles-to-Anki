9e725e775ebcea35d1cd52f30533eac8c916aa6f
Justin Brown <jvwbrown0@gmail.com>
2026-01-18 14:59:26 +0100

    first imports and comments

--- App/Main.py
+++ App/Main.py
@@ -1,3 +1,13 @@
-print("Lets create some subtitles babyyyyy")
-"We will be using whisper for this task"
+import os
+import sys
+import argparse
+import queue
+import Sounddevice as sd
+import numpy as np
+
+
+'first we need to install Sounddevice to record audio from the screen,'
+' then numPy to save audio data as an array,'
+' and finally we will install Whisper to transcribe the audio into text'
+'all this in a virtual environment to keep our dependencies organized'
 
