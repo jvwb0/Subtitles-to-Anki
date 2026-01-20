c16a2f713f1dd461d29f8280a15ae53f1cf0f927
Justin Brown <jvwbrown0@gmail.com>
2026-01-20 12:58:20 +0100

    resampleAudio method corrected the 48hz to 16hz

--- whisperTranscribeDemo.py
+++ whisperTranscribeDemo.py
@@ -8,7 +8,7 @@ DEVICE = 10
 RATE = 48000
 CHANNELS = 2
 CHUNK = 1024
-DURATION = 5
+DURATION = 10
 
 MODEL_SIZE = "tiny" 
 LANGUAGE = "pt" # Portuguese - we can change this as needed
@@ -22,7 +22,7 @@ def bytesToNumpyArray(data: bytes) -> np.ndarray:
 def transcribeAudio(audio: np.ndarray) -> None:
     model = WhisperModel(MODEL_SIZE, device="cpu",compute_type="int8",)
 
-    segments, info = model.transcribe(audio)
+    segments, info = model.transcribe(audio, beam_size=5)
 
     print("Detected language:", info.language)
     for segment in segments:
@@ -31,7 +31,19 @@ def transcribeAudio(audio: np.ndarray) -> None:
 def loadWavAsBytes(filename: str) -> bytes:
     with wave.open(filename, "rb") as wf:
         return wf.readframes(wf.getnframes())
+    
+def resampleAudio(audio: np.ndarray, srcRate: int) -> np.ndarray:
+    if srcRate == 16000:
+        return audio
+
+    xOld = np.linspace(0, len(audio) / srcRate, num=len(audio), endpoint=False)
+    nNew = int(len(audio) * 16000 / srcRate)
+    xNew = np.linspace(0, len(audio) / srcRate, num=nNew, endpoint=False)
+
+    return np.interp(xNew, xOld, audio).astype(np.float32)
+
 
 data = loadWavAsBytes("test_1768909877.wav")
 audio = bytesToNumpyArray(data)
+audio = resampleAudio(audio, RATE)
 transcribeAudio(audio)
