01d444c51cbb4ff8e51e9d7a01fecfb7ad9bf12f
Justin Brown <jvwbrown0@gmail.com>
2026-01-20 11:51:18 +0100

    fixed demo file naming through time stamps

--- App/AudioCaptureDemo.py
+++ App/AudioCaptureDemo.py
@@ -1,7 +1,5 @@
-#import sounddevice as sd
-from calendar import c
-from ctypes.util import test
 import numpy 
+import time
 import wave
 from faster_whisper import WhisperModel
 import pyaudiowpatch #because sounddevice doesnt supprot loopback recording on windows WASAPI
@@ -17,10 +15,9 @@ RATE = 48000
 CHANNELS = 2
 CHUNK = 1024
 DEVICE = 10  # Speakers : python -m pyaudiowpatch (to list devices)
-frames = []
 
-counter = 1
-filename = f"test{counter}.wav"
+#filename = f"test{counter}.wav"
+filename = f"test_{int(time.time())}.wav"
 
 def openLoopbackStream(p: pyaudiowpatch.PyAudio):
     return p.open(
@@ -37,13 +34,14 @@ def recordFrames(stream, durationSec: int) -> list[bytes]:
         frames.append(stream.read(CHUNK))
     return frames
 
-def saveWav(filename: str, frames: list[bytes]) -> None:
-    counter += 1
+def saveWav(frames: list[bytes]) -> None:
     with wave.open(filename, "wb") as wf:
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(2)  # 16-bit
         wf.setframerate(RATE)
         wf.writeframes(b"".join(frames))
+
+    print("Saved WAV file", filename)
 
 def main():
     p = pyaudiowpatch.PyAudio()
@@ -57,6 +55,5 @@ def main():
     p.terminate()
 
     saveWav(frames)
-    print("Saved WAV file:", filename)
 
 if __name__ == "__main__": main()
