9baf7db97f79502c5586ce8d844061416aaa4c6a
Justin Brown <jvwbrown0@gmail.com>
2026-01-20 12:54:00 +0100

    tested transcription method with drake song. lyrcics came out wrong, need to fix Hz quality and compression & music interference

--- whisperTranscribeDemo.py
+++ whisperTranscribeDemo.py
@@ -10,11 +10,28 @@ CHANNELS = 2
 CHUNK = 1024
 DURATION = 5
 
-MODEL_SIZE = "tiny"
+MODEL_SIZE = "tiny" 
 LANGUAGE = "pt" # Portuguese - we can change this as needed
 
 def bytesToNumpyArray(data: bytes) -> np.ndarray:
     audio = np.frombuffer(data, dtype=np.int16)
-    audio = np.reshape(audio, (-1, CHANNELS).mean(axis=1))  # convert to mono by averaging channels
+    audio = audio.reshape(-1, CHANNELS).mean(axis=1)  # stereo → mono
     audio = audio.astype(np.float32) / 32768.0
     return audio
+
+def transcribeAudio(audio: np.ndarray) -> None:
+    model = WhisperModel(MODEL_SIZE, device="cpu",compute_type="int8",)
+
+    segments, info = model.transcribe(audio)
+
+    print("Detected language:", info.language)
+    for segment in segments:
+        print(segment.text)
+
+def loadWavAsBytes(filename: str) -> bytes:
+    with wave.open(filename, "rb") as wf:
+        return wf.readframes(wf.getnframes())
+
+data = loadWavAsBytes("test_1768909877.wav")
+audio = bytesToNumpyArray(data)
+transcribeAudio(audio)
