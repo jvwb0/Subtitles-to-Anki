e198f91c1b766eda6c8b37f6ed09329395c18491
Justin Brown <jvwbrown0@gmail.com>
2026-01-23 10:41:48 +0100

    -user selects output device

--- App/main_live.py
+++ App/main_live.py
@@ -1,21 +1,30 @@
+import subprocess
+import sys
 import time
 from Controller import TranscriptionController
 
-controller = TranscriptionController("small", device=10)
+print("\nListing available audio devices (WASAPI loopback):\n")
+
+# run: python -m pyaudiowpatch
+subprocess.run([sys.executable, "-m", "pyaudiowpatch"])
+    
+print(
+    "\nPlease choose which device to use for loopback recording\n"
+    "(Look for WASAPI devices with LOOPBACK = True)\n"
+)
+
+device_str = input("Enter device index (number): ")
+DEVICE = int(device_str.strip())
+controller = TranscriptionController("small", device=DEVICE) #sizes: tiny, base, small, medium, large
 
 print("Recording 10 seconds... (live mode)")
 controller.startLive("live.wav")
 
 start = time.time()
-last_print = start
 
 while time.time() - start < 10:
     controller.liveTick()  # record one chunk
-
-    # print elapsed every 0.5 sec so you see it running
-start = time.time()
-while time.time() - start < 10:
-    controller.liveTick()  # record one chunk
+    time.sleep(0.01) 
 
 print("Stopping + transcribing...")
 words = controller.stopLiveAndTranscribe()
